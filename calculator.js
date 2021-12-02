845a39e75b38a167c5b59e3e4f133a40529cb101
Yoann Perez <yoann.perez@gmail.com>
2021-12-02 21:38:41 +0100

    Finish calculator.js

--- calculator.js
+++ calculator.js
@@ -1,33 +1,85 @@
-const keys = document.querySelectorAll(".roundBtn");
-const display = document.getElementById("displayTxt");
-let keyOne = document.getElementById("one");
+"use strict";
 
-let elemA = [];
-var dotValidate = true;
-var clearOne = false;
+const keys = document.querySelectorAll(".pad");
+const operand = document.querySelectorAll(".operand");
+const evaluate = document.querySelector(".evaluate");
+const display = document.getElementById("displayTxt");
+
+let currentValue;
+var firstValue;
+var dotValidate;
+var operateur;
+
+const init = () => {
+  currentValue = [];
+  firstValue = 0;
+  dotValidate = true;
+  operateur = "";
+};
+
+init();
+
+function storeFirstValue() {
+  firstValue = currentValue.join("");
+}
 
 keys.forEach((e) => {
   e.addEventListener("click", function () {
     if (e.id == "cancel") {
       // Cancel Case
-      elemA = [];
+      currentValue = [];
       dotValidate = true;
       display.innerText = 0;
-    } else {// Dot case
+    } else {
+      // Dot case
       if (e.id == "dot" && dotValidate) {
         dotValidate = false;
-        console.log("Coucou dot");
-        elemA.push(".");
-        display.innerText = elemA.join("");
+        currentValue.push(".");
+        display.innerText = currentValue.join("");
       }
       if (e.id == "dot" && !dotValidate) {
         console.log("Dot non valide");
-      } else { // Number case
-        elemA.push(parseInt(e.id));
-        display.innerText = elemA.join("");
+      } else {
+        // Number case
+        currentValue.push(parseInt(e.id));
+        display.innerText = currentValue.join("");
       }
     }
   });
 });
 
-//
+operand.forEach((op) => {
+  op.addEventListener("click", function () {
+    switch (op.id) {
+      case "div":
+        storeFirstValue();
+        operateur = "/";
+        currentValue = [];
+        break;
+      case "multi":
+        storeFirstValue();
+        operateur = "*";
+        currentValue = [];
+        break;
+      case "minus":
+        storeFirstValue();
+        operateur = "-";
+        currentValue = [];
+        break;
+      case "more":
+        storeFirstValue();
+        operateur = "+";
+        currentValue = [];
+        break;
+      case "eval":
+        if (firstValue != 0) {
+          let result = eval(`${firstValue} ${operateur} ${currentValue.join("")}`);
+          result.toString().length > 9 ? (display.innerText = result.toFixed(7)) : (display.innerText = result);
+        }
+        init();
+        break;
+      default:
+        console.log("error");
+    }
+  });
+});
