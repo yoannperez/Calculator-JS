d8bee055552bf9da9700e3a288cd236587ea233c
Yoann Perez <yoann.perez@gmail.com>
2021-12-11 23:51:36 +0100

    Cleanup

--- calculator.js
+++ calculator.js
@@ -7,19 +7,14 @@ const memKey = document.querySelectorAll(".mem");
 const display = document.getElementById("displayTxt");
 
 let currentValue = [];
-var firstValue;
 var dotValidate;
-var operateur;
 var memory = 0;
 let cancelAlreadyPress = false;
 let result = 0;
 let prevOperation = "";
 
 const init = () => {
-  // currentValue ;
-  firstValue = 0;
   dotValidate = true;
-  operateur = "";
 };
 
 init();
@@ -54,23 +49,13 @@ const calculator = (a, b, operator) => {
 };
 
 function launchCalculate(operator) {
-  console.log("operator = " + operator);
   if (prevOperation.toString().length == 0) {
-    console.log("première opération");
     result = parseFloat(currentValue.join(""));
     prevOperation = operator;
-    
-    // result = calculator(parseFloat(currentValue.join("")),result, operator);
-    // console.log("result = " + result);
   } else {
-    console.log("currentValue : " + parseFloat(currentValue.join("")));
-    console.log("Pas première opération");
     result = calculator(result, parseFloat(currentValue.join("")), prevOperation);
     prevOperation = operator;
     printResult();
-    console.log("result = " + result);
-    console.log("prevOperator : " + prevOperation);
-    console.log("currentValue : " + currentValue);
   }
 
   currentValue = [];
@@ -82,13 +67,11 @@ keys.forEach((e) => {
     switch (e.id) {
       case "cancel":
         if (cancelAlreadyPress === false) {
-          console.log("première pression");
           currentValue = [];
           display.innerText = 0;
           dotValidate = true;
           cancelAlreadyPress = true;
         } else {
-          console.log("Reset total");
           currentValue = [];
           result = 0;
           dotValidate = true;
@@ -129,19 +112,12 @@ operand.forEach((op) => {
         break;
       case "eval":
         result = calculator(result, parseFloat(currentValue.join("")), prevOperation);
-        let myFunc = num => Number(num)
+        let myFunc = (num) => Number(num);
         currentValue = Array.from(String(result), myFunc);
         prevOperation = "";
 
-        console.log("currentValue : " + currentValue);
-        console.log("currentValue : " + currentValue.join(""));
-        console.log("currentValue : " + parseFloat(currentValue.join("")));
-        display.innerText = result
-        console.log("result = " + result);
-        if (firstValue != 0) {
-          // let result = eval(`${firstValue} ${operateur} ${currentValue.join("")}`);
-          // result.toString().length > 9 ? (display.innerText = result.toFixed(9)) : (display.innerText = result);
-        }
+        display.innerText = result;
+
         init();
         break;
       default:
